017b41fbe99bee92df91615a805ae31e6345df48
srg <skabass@yandex.ru>
2020-04-16 11:01:39 +0300

    3.1 separate data from movie-card and modal markup into variables

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,4 +1,34 @@
 export const createModalTemplate = () => {
+  // Данные фильма
+  const poster = `./images/posters/the-great-flamarion.jpg`;
+  const contentRating = 18;
+  const title = `The Great Flamarion`;
+  const originTitle = `The Great Flamarion`;
+  const rating = 8.9;
+  const director = `Anthony Mann`;
+  const writers = `Anne Wigton, Heinz Herald, Richard Weil`;
+  const actors = `Erich von Stroheim, Mary Beth Hughes, Dan Duryea`;
+  const date = `30 March 1945`;
+  const duration = `1h 59m`;
+  const country = `USA`;
+  const genres = [`Drama`, `Film-Noir`, `Mystery`]; // add change name
+  const description = `The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.`;
+  const onWatchlist = false;
+  const isWatched = false;
+  const onFavorite = false;
+
+  const ratingForInsertion = rating % 1 ? rating : rating + `.0`;
+
+  // Данные комментариев
+  const emoji = [
+    `./images/emoji/smile.png`,
+    `./images/emoji/sleeping.png`,
+    `./images/emoji/puke.png`,
+    `./images/emoji/angry.png`];
+  const text = [`Interesting setting and a good cast`, `Booooooooooring`, `Very very old. Meh`, `Almost two hours? Seriously?`];
+  const author = [`Tim Macoveev`, `John Doe`, `John Doe`, `John Doe`];
+  const dateComment = [`2019/12/31 23:59`, `2 days ago`, `2 days ago`, `Today`];
+
   return (
     `<section class="film-details">
       <form class="film-details__inner" action="" method="get">
@@ -8,71 +38,69 @@ export const createModalTemplate = () => {
           </div>
           <div class="film-details__info-wrap">
             <div class="film-details__poster">
-              <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+              <img class="film-details__poster-img" src="${poster}" alt="film poster ${title}">
 
-              <p class="film-details__age">18+</p>
+              <p class="film-details__age">${contentRating}+</p>
             </div>
 
             <div class="film-details__info">
               <div class="film-details__info-head">
                 <div class="film-details__title-wrap">
-                  <h3 class="film-details__title">The Great Flamarion</h3>
-                  <p class="film-details__title-original">Original: The Great Flamarion</p>
+                  <h3 class="film-details__title">${title}</h3>
+                  <p class="film-details__title-original">Original: ${originTitle}</p>
                 </div>
 
                 <div class="film-details__rating">
-                  <p class="film-details__total-rating">8.9</p>
+                  <p class="film-details__total-rating">${ratingForInsertion}</p>
                 </div>
               </div>
 
               <table class="film-details__table">
                 <tr class="film-details__row">
                   <td class="film-details__term">Director</td>
-                  <td class="film-details__cell">Anthony Mann</td>
+                  <td class="film-details__cell">${director}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Writers</td>
-                  <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+                  <td class="film-details__cell">${writers}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Actors</td>
-                  <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+                  <td class="film-details__cell">${actors}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Release Date</td>
-                  <td class="film-details__cell">30 March 1945</td>
+                  <td class="film-details__cell">${date}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Runtime</td>
-                  <td class="film-details__cell">1h 18m</td>
+                  <td class="film-details__cell">${duration}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Country</td>
-                  <td class="film-details__cell">USA</td>
+                  <td class="film-details__cell">${country}</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">Genres</td>
+                  <td class="film-details__term">${genres.length === 1 ? `Genre` : `Genres`}</td>
                   <td class="film-details__cell">
-                    <span class="film-details__genre">Drama</span>
-                    <span class="film-details__genre">Film-Noir</span>
-                    <span class="film-details__genre">Mystery</span></td>
+                    <span class="film-details__genre">${genres[0]}</span>
+                    <span class="film-details__genre">${genres[1]}</span>
+                    <span class="film-details__genre">${genres[2]}</span></td>
                 </tr>
               </table>
 
-              <p class="film-details__film-description">
-                The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
-              </p>
+              <p class="film-details__film-description">${description}</p>
             </div>
           </div>
 
           <section class="film-details__controls">
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
+            <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist" ${onWatchlist ? `checked` : ``}>
             <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
+            <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched" ${isWatched ? `checked` : ``}>
             <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
+            <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite" ${onFavorite ? `checked` : ``}>
             <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
           </section>
         </div>
@@ -84,52 +112,52 @@ export const createModalTemplate = () => {
             <ul class="film-details__comments-list">
               <li class="film-details__comment">
                 <span class="film-details__comment-emoji">
-                  <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
+                  <img src="${emoji[0]}" width="55" height="55" alt="emoji-smile">
                 </span>
                 <div>
-                  <p class="film-details__comment-text">Interesting setting and a good cast</p>
+                  <p class="film-details__comment-text">${text[0]}</p>
                   <p class="film-details__comment-info">
-                    <span class="film-details__comment-author">Tim Macoveev</span>
-                    <span class="film-details__comment-day">2019/12/31 23:59</span>
+                    <span class="film-details__comment-author">${author[0]}</span>
+                    <span class="film-details__comment-day">${dateComment[0]}</span>
                     <button class="film-details__comment-delete">Delete</button>
                   </p>
                 </div>
               </li>
               <li class="film-details__comment">
                 <span class="film-details__comment-emoji">
-                  <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji-sleeping">
+                  <img src="${emoji[1]}" width="55" height="55" alt="emoji-sleeping">
                 </span>
                 <div>
-                  <p class="film-details__comment-text">Booooooooooring</p>
+                  <p class="film-details__comment-text">${text[1]}</p>
                   <p class="film-details__comment-info">
-                    <span class="film-details__comment-author">John Doe</span>
-                    <span class="film-details__comment-day">2 days ago</span>
+                    <span class="film-details__comment-author">${author[1]}</span>
+                    <span class="film-details__comment-day">${dateComment[1]}</span>
                     <button class="film-details__comment-delete">Delete</button>
                   </p>
                 </div>
               </li>
               <li class="film-details__comment">
                 <span class="film-details__comment-emoji">
-                  <img src="./images/emoji/puke.png" width="55" height="55" alt="emoji-puke">
+                  <img src="${emoji[2]}" width="55" height="55" alt="emoji-puke">
                 </span>
                 <div>
-                  <p class="film-details__comment-text">Very very old. Meh</p>
+                  <p class="film-details__comment-text">${text[2]}</p>
                   <p class="film-details__comment-info">
-                    <span class="film-details__comment-author">John Doe</span>
-                    <span class="film-details__comment-day">2 days ago</span>
+                    <span class="film-details__comment-author">${author[2]}</span>
+                    <span class="film-details__comment-day">${dateComment[2]}</span>
                     <button class="film-details__comment-delete">Delete</button>
                   </p>
                 </div>
               </li>
               <li class="film-details__comment">
                 <span class="film-details__comment-emoji">
-                  <img src="./images/emoji/angry.png" width="55" height="55" alt="emoji-angry">
+                  <img src="${emoji[3]}" width="55" height="55" alt="emoji-angry">
                 </span>
                 <div>
-                  <p class="film-details__comment-text">Almost two hours? Seriously?</p>
+                  <p class="film-details__comment-text">${text[3]}</p>
                   <p class="film-details__comment-info">
-                    <span class="film-details__comment-author">John Doe</span>
-                    <span class="film-details__comment-day">Today</span>
+                    <span class="film-details__comment-author">${author[3]}</span>
+                    <span class="film-details__comment-day">${dateComment[3]}</span>
                     <button class="film-details__comment-delete">Delete</button>
                   </p>
                 </div>
@@ -146,22 +174,22 @@ export const createModalTemplate = () => {
               <div class="film-details__emoji-list">
                 <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
                 <label class="film-details__emoji-label" for="emoji-smile">
-                  <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
+                  <img src="${emoji[0]}" width="30" height="30" alt="emoji">
                 </label>
 
                 <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
                 <label class="film-details__emoji-label" for="emoji-sleeping">
-                  <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
+                  <img src="${emoji[1]}" width="30" height="30" alt="emoji">
                 </label>
 
                 <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
                 <label class="film-details__emoji-label" for="emoji-puke">
-                  <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
+                  <img src="${emoji[2]}" width="30" height="30" alt="emoji">
                 </label>
 
                 <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
                 <label class="film-details__emoji-label" for="emoji-angry">
-                  <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
+                  <img src="${emoji[3]}" width="30" height="30" alt="emoji">
                 </label>
               </div>
             </div>
--- src/components/movie-card.js
+++ src/components/movie-card.js
@@ -1,20 +1,37 @@
 export const createMovieCardTemplate = () => {
+  const title = `The Man with the Golden Arm`;
+  const rating = 9;
+  const year = 1955; // вычисляется из даты
+  const duration = `1h 59m`;
+  const genres = [`Drama`];
+  const poster = `./images/posters/the-man-with-the-golden-arm.jpg`;
+  const description = `Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…`;
+  const onWatchlist = false;
+  const isWatched = true;
+  const onFavorite = false;
+
+  const ratingForInsertion = rating % 1 ? rating : rating + `.0`;
+  const descriptionForInsertion = description.length > 140 ? (description.substring(0, 139) + `...`) : description;
+  const watchlistButtonActiveClass = onWatchlist ? `film-card__controls-item--active` : ``;
+  const watchedButtonActiveClass = isWatched ? `film-card__controls-item--active` : ``;
+  const favoriteButtonActiveClass = onFavorite ? `film-card__controls-item--active` : ``;
+
   return (
     `<article class="film-card">
-      <h3 class="film-card__title">The Man with the Golden Arm</h3>
-      <p class="film-card__rating">9.0</p>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${ratingForInsertion}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1955</span>
-        <span class="film-card__duration">1h 59m</span>
-        <span class="film-card__genre">Drama</span>
+        <span class="film-card__year">${year}</span>
+        <span class="film-card__duration">${duration}</span>
+        <span class="film-card__genre">${genres[0]}</span>
       </p>
-      <img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
+      <img src="${poster}" alt="film poster ${title}" class="film-card__poster">
+      <p class="film-card__description">${descriptionForInsertion}</p>
       <a class="film-card__comments">18 comments</a>
       <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  film-card__controls-item--active">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${watchlistButtonActiveClass}">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  ${watchedButtonActiveClass}">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${favoriteButtonActiveClass}">Mark as favorite</button>
       </form>
     </article>`
   );
