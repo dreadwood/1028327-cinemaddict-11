2fd69619aecc6e6a6d6d7e9da6981717db006415
srg <skabass@yandex.ru>
2020-04-21 01:04:00 +0300

    3.5 create generation for sorting

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,9 +1,16 @@
+const createSortingMarkup = (type, isActive) => {
+  return (
+    `<li><a href="#" class="sort__button ${isActive ? `sort__button--active` : ``}">${type}</a></li>`
+  );
+};
+
 export const createSortingTemplate = () => {
+  const sortTypes = [`Sort by default`, `Sort by date`, `Sort by rating`];
+  const sortingMarkup = sortTypes.map((it, i) => createSortingMarkup(it, i === 0)).join(`\n`);
+
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      ${sortingMarkup}
     </ul>`
   );
 };
