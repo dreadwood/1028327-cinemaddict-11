387276f68b53d79382bfe0bd00f1a408e47ee35b
srg <skabass@yandex.ru>
2020-05-02 02:50:31 +0300

    5.2.2 add sorting function

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -1,17 +1,19 @@
+import Navigation from '../components/navigation.js';
 import NoData from '../components/no-data.js';
+import Sorting, {SortTypes} from '../components/sorting.js';
 import FilmContainer from '../components/film-container.js';
 import FilmCard from '../components/film-card.js';
 import ShowMoreButton from '../components/show-more-button.js';
 import FilmDetails from '../components/film-details.js';
 import {render, remove} from '../utils/render.js';
-import {getSortedFilms} from '../utils/film-utils.js';
+import {getSortedFilms, countFilms} from '../utils/film-utils.js';
 
 const TOP_FILM_COUNT = 2;
 const COMMENTED_FILM_COUNT = 2;
 const FILM_COUNT_ON_START = 5;
 const FILM_COUNT_BY_BUTTON = 5;
 
-const SortType = {
+const FilmListType = {
   RATING: `rating`,
   COMMENTS: `comments`,
 };
@@ -20,20 +22,48 @@ export default class PageController {
   constructor(container) {
     this._container = container;
 
+    this._sortingComponent = new Sorting();
     this._noDataComponent = new NoData();
     this._filmContainerComponent = new FilmContainer();
     this._showMoreButtonComponent = new ShowMoreButton();
+    this._sortingComponent = new Sorting();
 
     this._bodyElement = document.querySelector(`body`);
     this._openedFilmDetails = false;
   }
 
   render(movies) {
+    const quantity = countFilms(movies);
+    const navigationComponent = new Navigation(quantity);
+    render(this._container, navigationComponent);
+    render(this._container, this._sortingComponent);
+
     const isDataBaseEmpty = movies.length === 0 ? true : false;
     if (isDataBaseEmpty) {
       render(this._container, this._noDataComponent);
       return;
     }
+
+    const renderShowMoreButton = () => {
+      if (showingFilmCount >= movies.length) {
+        return;
+      }
+
+      render(filmListElement, this._showMoreButtonComponent);
+
+      this._showMoreButtonComponent.setClickHandler((evt) => {
+        evt.preventDefault();
+        const prevFilmCount = showingFilmCount;
+        showingFilmCount += FILM_COUNT_BY_BUTTON;
+
+        const sortedMovies = this._getSortedMovies(movies, this._sortingComponent.getSortType(), prevFilmCount, showingFilmCount);
+        this._renderFilms(filmListContainerElement, sortedMovies);
+
+        if (showingFilmCount >= movies.length) {
+          remove(this._showMoreButtonComponent);
+        }
+      });
+    };
 
     render(this._container, this._filmContainerComponent);
 
@@ -41,28 +71,23 @@ export default class PageController {
     const filmListContainerElement = filmListElement.querySelector(`.films-list__container`);
 
     let showingFilmCount = FILM_COUNT_ON_START;
-    movies.slice(0, showingFilmCount).forEach((movie) => {
-      this._renderFilm(filmListContainerElement, movie);
+
+    this._renderFilms(filmListContainerElement, movies.slice(0, showingFilmCount));
+    renderShowMoreButton();
+
+    this._sortingComponent.setSortTypeChangeHandler((sortType) => {
+      showingFilmCount = FILM_COUNT_ON_START;
+
+      const sortedMovies = this._getSortedMovies(movies, sortType, 0, showingFilmCount);
+
+      filmListContainerElement.innerHTML = ``;
+
+      this._renderFilms(filmListContainerElement, sortedMovies);
+      renderShowMoreButton();
     });
 
-    render(filmListElement, this._showMoreButtonComponent);
-
-    this._showMoreButtonComponent.setClickHandler((evt) => {
-      evt.preventDefault();
-      const prevFilmCount = showingFilmCount;
-      showingFilmCount += FILM_COUNT_BY_BUTTON;
-
-      movies.slice(prevFilmCount, showingFilmCount).forEach((movie) => {
-        this._renderFilm(filmListContainerElement, movie);
-      });
-
-      if (showingFilmCount >= movies.length) {
-        remove(this._showMoreButtonComponent);
-      }
-    });
-
-    const topFilms = getSortedFilms(movies, SortType.RATING);
-    const commentedFilms = getSortedFilms(movies, SortType.COMMENTS);
+    const topFilms = getSortedFilms(movies, FilmListType.RATING);
+    const commentedFilms = getSortedFilms(movies, FilmListType.COMMENTS);
     const topFilmListElement = this._filmContainerComponent.getElement().querySelector(`.films-list--top .films-list__container`);
     const commentedFilmListElement = this._filmContainerComponent.getElement().querySelector(`.films-list--commented .films-list__container`);
     this._repeatRender(topFilmListElement, topFilms, TOP_FILM_COUNT);
@@ -118,5 +143,28 @@ export default class PageController {
     for (let i = 0; i < iterations; i++) {
       this._renderFilm(container, movies[i]);
     }
+  }
+
+  _renderFilms(container, films) {
+    films.forEach((film) => this._renderFilm(container, film));
+  }
+
+  _getSortedMovies(films, sortType, from, to) {
+    let sortedMovies = [];
+    const swowingMovies = films.slice();
+
+    switch (sortType) {
+      case SortTypes.get(`Sort by date`):
+        sortedMovies = swowingMovies.sort((a, b) => b[sortType] - a[sortType]);
+        break;
+      case SortTypes.get(`Sort by rating`):
+        sortedMovies = swowingMovies.sort((a, b) => b[sortType] - a[sortType]);
+        break;
+      case SortTypes.get(`Sort by default`):
+        sortedMovies = swowingMovies;
+        break;
+    }
+
+    return sortedMovies.slice(from, to);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,19 @@
 import UserProfile from './components/user-profile.js';
-import Navigation from './components/navigation.js';
-import Sorting from './components/sorting.js';
 import PageController from './controllers/page.js';
 import {getRandomInteger} from './utils/common.js';
 import {render} from './utils/render.js';
-import {countFilms} from './utils/film-utils.js';
 import {generateFilms} from './mock/film.js';
 
 const FILM_COUNT = 18;
-
 const films = generateFilms(FILM_COUNT);
-const quantity = countFilms(films);
+
 const isDataBaseEmpty = films.length === 0 ? true : false;
 
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 
 const userProfileComponent = new UserProfile();
-const navigationComponent = new Navigation(quantity);
-const sortingComponent = new Sorting();
-
 render(headerElement, userProfileComponent);
-render(mainElement, navigationComponent);
-render(mainElement, sortingComponent);
 
 const pageController = new PageController(mainElement);
 pageController.render(films);
